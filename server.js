27e8463bb257f1e9cf7c075a66586ed9c7126d1d
Julio.S <just.julio95@gmail.com>
2022-06-26 12:32:38 -0400

    began work on the homepage of blog site

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
+const path = require('path');
+//const helpers = require('./utils/helpers');
 const express = require('express');
-const routes = require('./controllers')
-const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const sequelize = require('./config/connection')
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess))
 
+//turn on routes
 app.use(routes);
 
+//turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('LISTENING!'))
+    app.listen(PORT, () => console.log('Now listening!'))
 })
